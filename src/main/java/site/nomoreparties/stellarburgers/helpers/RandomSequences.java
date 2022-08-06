2d7a37bedb5536cf9aee1ccc05b5c00359e10a75
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-08-06 23:37:58 +0300

    Add random values generators

---
+++ src/main/java/site/nomoreparties/stellarburgers/helpers/RandomSequences.java
@@ -0,0 +1,33 @@
+package site.nomoreparties.stellarburgers.helpers;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.UUID;
+
+public class RandomSequences {
+
+    private static final Faker ruFaker = new Faker(new Locale("ru"));
+    private static final Faker enFaker = new Faker(new Locale("en"));
+
+    public static String createRandomUuid() {
+        return String.valueOf(UUID.randomUUID());
+    }
+
+    public static String createRandomPassword(int length) {
+        String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%&";
+        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+
+    public static String getRandomName() {
+        return ruFaker.name().firstName() + createRandomPassword(3);
+    }
+
+    public static String getRandomEmail () { return createRandomPassword(3) + enFaker.internet().safeEmailAddress(); }
+}
