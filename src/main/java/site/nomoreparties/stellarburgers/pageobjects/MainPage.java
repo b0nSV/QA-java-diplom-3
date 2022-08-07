6ca5efe3efefb5ea6f3ca55004a6f3426328db5c
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-08-07 16:10:27 +0300

    Add selectors for elements

--- src/main/java/site/nomoreparties/stellarburgers/pageobjects/MainPage.java
+++ src/main/java/site/nomoreparties/stellarburgers/pageobjects/MainPage.java
@@ -1,6 +1,7 @@
 package site.nomoreparties.stellarburgers.pageobjects;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import lombok.Getter;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -31,4 +32,59 @@ public class MainPage {
     @FindBy(how = How.XPATH, using = ".//h1[text()='Соберите бургер']")
     @Getter
     private SelenideElement pageHeader;
+
+    // Заголовок "Булки" внутри панели ингредиентов
+    @FindBy(how = How.XPATH, using = ".//h2[text()='Булки']")
+    @Getter
+    private SelenideElement bunInsideHeader;
+
+    // Заголовок "Соусы" внутри панели ингредиентов
+    @FindBy(how = How.XPATH, using = ".//h2[text()='Соусы']")
+    @Getter
+    private SelenideElement saucesInsideHeader;
+
+    // Заголовок "Начинки" внутри панели ингредиентов
+    @FindBy(how = How.XPATH, using = ".//h2[text()='Начинки']")
+    @Getter
+    private SelenideElement fillingInsideHeader;
+
+    // Последний элемент в панели ингредиентов, раздела "Начинки"
+    @FindBy(how = How.XPATH, using = "(.//div[@class='BurgerIngredients_ingredients__menuContainer__Xu3Mo']/ul[3]/a)[last()]")
+    @Getter
+    private SelenideElement fillingLastElement;
+
+    // Заголовок раздела "Булки"
+    @FindBy(how = How.XPATH, using = ".//span[text()='Булки']/..")
+    @Getter
+    private SelenideElement bunHeader;
+
+    // Заголовок раздела "Соусы"
+    @FindBy(how = How.XPATH, using = ".//span[text()='Соусы']")
+    @Getter
+    private SelenideElement sauceHeader;
+
+    // Заголовок раздела "Начинки"
+    @FindBy(how = How.XPATH, using = ".//span[text()='Начинки']")
+    @Getter
+    private SelenideElement fillingHeader;
+
+    @Step("Прокрутить панель ингредиентов в самый низ")
+    public void scrollToLastIngredient() {
+        fillingLastElement.scrollIntoView(true);
+    }
+
+    @Step("Нажать на заголовок раздела \"Булки\"")
+    public void clickBunHeader() {
+        bunHeader.click();
+    }
+
+    @Step("Нажать на заголовок раздела \"Соусы\"")
+    public void clickSauceHeader() {
+        sauceHeader.click();
+    }
+
+    @Step("Нажать на заголовок раздела \"Начинки\"")
+    public void clickFillingHeader() {
+        fillingHeader.click();
+    }
 }
